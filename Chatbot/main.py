4374be27f918230d8046d58d0954714692ae4c22
deepak kaushik <deepak.kaushik161@gmail.com>
2020-04-03 20:05:18 +0530

    Bug Fixes and Snowboy Added

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -4,11 +4,12 @@ import requests
 import json
 import os
 import datetime
+import snowboydecoder
+from threading import Thread
 
 
 #Few basic Configurations:
 
-wake_word = False
 whether_api = ''
 
 
@@ -19,67 +20,64 @@ rec = Recognizer()
 class Assistant(object):
     
     def __init__(self):
-        print("initializing")
+        print("Initializing")
         os.system('aplay /home/pi/Desktop/Smart-Mirror-Chatbot/Audio-Files/Startup-Female.wav')
+        
+        self.wake_word = False
+        
+        self.detector = snowboydecoder.HotwordDetector("/home/pi/Desktop/Smart-Mirror-Chatbot/Audio-Files/smart_mirror.pmdl", 
+                       sensitivity = 0.5, audio_gain = 1)
+       
+        
+    def __wakeword_callback(self):
+        self.__start_listening()
+    
+    
+    def __detect_hotword(self):
+        self.detector.start(self.__wakeword_callback)
     
     def start(self):
-        wake_word = False
-        while True:
-            #print('Listening')
-            userInput = rec.recognize()
-            if userInput is not None:  
-                query_result = chatter.chat(userInput)
-
-            #Parsing information from response
-                input_text = query_result.query_text
-                # output = query_result.fulfillment_text
-                intent = query_result.intent.display_name
-                print(input_text, intent)
-                # check is ok mirror is said or not
-                if  intent == 'wake word':
-                    wake_word = True
-            if wake_word:
-                os.system(r'aplay /home/pi/Desktop/Smart-Mirror-Chatbot/Audio-Files/Fb.wav')
-                requests.get("http://localhost:8080/clear")
-                print('Speak Now')
-                userInput = rec.recognize()
-                #print(userInput)
-            
-                if userInput is not None:  
-                    query_result = chatter.chat(userInput)
+        self.__detect_hotword()
         
-                    #Parsing information from response
-                    input_text = query_result.query_text
-                    output = query_result.fulfillment_text
-                    intent = query_result.intent.display_name
-                    requests.get("http://localhost:8080/statement?text=%s" % output)
-                    print('you said : {}\nbot said:{}'.format(input_text,output))
-                    rec.say(output)
-                    
-                    
-                    #if intent == 'Time':
-                        
-                    #if intent == 'faculty information':
-                        
-                    if intent == 'faculty photograph':
-                        im_url = '/home/pi/Downloads/ECE_department.jpg'
-                        requests.get("http://localhost:8080/clear")
-                        body = {'url':im_url}
-                        #requests.get("http://localhost:8080/face")
-                        requests.get("http://localhost:8080/image?text=%s" % im_url)
+    def __start_listening(self):
+        os.system(r'aplay /home/pi/Desktop/Smart-Mirror-Chatbot/Audio-Files/Fb.wav')
+        print('Speak Now')
+        userInput = rec.recognize()
+        requests.get("http://localhost:8080/clear")
+        if userInput is not None:  
+            query_result = chatter.chat(userInput)
+            #Parsing information from response
+            input_text = query_result.query_text
+            output = query_result.fulfillment_text
+            intent = query_result.intent.display_name
+            requests.get("http://localhost:8080/statement?text=%s" % output)
+            print('you said : {}\nbot said:{}'.format(input_text,output))
+            rec.say(output)
+            
+            
+            #if intent == 'Time':
+                
+            #if intent == 'faculty information':
+                
+            if intent == 'faculty photograph':
+                im_url = '/home/pi/Downloads/ECE_department.jpg'
+                requests.get("http://localhost:8080/clear")
+                body = {'url':im_url}
+                #requests.get("http://localhost:8080/face")
+                requests.get("http://localhost:8080/image?text=%s" % im_url)
 
-                        
-                    #if intent = 'time table faculty':
-                    
-                    #if intent = 'time table class':
-                    
-                    #if intent = 'weather':
-                        
-                    
-                    
-                    
-                    if not intent == 'Default Fallback Intent':
-                        wake_word = False
+                
+            #if intent = 'time table faculty':
+            
+            #if intent = 'time table class':
+            
+            #if intent = 'weather':
+                
+            
+            
+            
+            if not intent == 'Default Fallback Intent':
+                wake_word = False
 
 
 
